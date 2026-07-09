c42c290ef82d5e17723e52e4e2b21fb682ff7215
Maximillian Schoch <maximillian.schoch@gmail.com>
2026-07-09 23:33:32 +0200

    Fix Minecraft 26.2 mandatory Item ID requirement in Properties

--- src/main/java/com/example/WitchHatMod.java
+++ src/main/java/com/example/WitchHatMod.java
@@ -5,6 +5,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.example.item.SpellPaperItem;
@@ -15,11 +17,12 @@ public class WitchHatMod implements ModInitializer {
     public static final String MOD_ID = "wha-magic";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Item SPELL_PAPER = new SpellPaperItem(new Item.Properties().stacksTo(1));
+    public static final ResourceKey<Item> SPELL_PAPER_KEY = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, "spell_paper"));
+    public static final Item SPELL_PAPER = new SpellPaperItem(new Item.Properties().setId(SPELL_PAPER_KEY).stacksTo(1));
 
     @Override
     public void onInitialize() {
-        Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, "spell_paper"), SPELL_PAPER);
+        Registry.register(BuiltInRegistries.ITEM, SPELL_PAPER_KEY, SPELL_PAPER);
         
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> {
             content.accept(SPELL_PAPER);
