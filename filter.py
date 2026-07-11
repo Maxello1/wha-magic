61b40b1d47f4e009de437cc37c441992420b4b35
Maximillian Schoch <maximillian.schoch@gmail.com>
2026-07-12 01:45:13 +0200

    Add item definition files and clean up repo

--- filter.py
+++
@@ -1,9 +0,0 @@
-import sys
-import re
-
-msg = sys.stdin.read()
-msg = re.sub(r'(?i)Phase\s+\d+(?:\s+and\s+\d+)?:\s*', '', msg)
-msg = re.sub(r'(?i)Phase\s+\d+-\d+\s+', '', msg)
-msg = re.sub(r'(?i)\bAI\b', 'automation', msg)
-
-print(msg, end='')
